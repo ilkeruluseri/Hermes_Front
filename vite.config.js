f3cda44b75b3d3b5fd4550926d6e7729a383bfe5
Ilker Uluseri <ilkeruluseri@hotmail.com>
2026-04-14 09:47:00 +0000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['team-041.hackaton.sivas.edu.tr'],
+    host: true,
+    port: 8000,
+  },
+  proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/docs': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/openapi.json': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+})
